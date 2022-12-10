df82507ae68e9e268396980ec9ec2f5ba4716775
MUHAMMAD IHSANUDIN SYAIFULLAH <111124211+IhsanudinS@users.noreply.github.com>
2022-12-10 17:38:31 +0700

    Add files via upload

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,6 +1,6 @@
 class DataSource {
   
-  static searchMeal(keyword) {
+  static searchRecipes(keyword) {
     
     return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
       .then(response =>{
